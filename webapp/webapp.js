5f82d69478269cab508f82b6c06b029ab034f0f3
tuananh250696 <35196486+tuananh250696@users.noreply.github.com>
2018-03-24 00:21:43 +0700

    Update webapp.js

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -20,65 +20,41 @@ angular.module('myApp', [
 	////Khu 1 -- Khu cài đặt tham số 
     //cài đặt một số tham số test chơi
 	//dùng để đặt các giá trị mặc định
-    $scope.CamBienMua = "Không biết nữa ahihi,dơi cập nhật";
+
 	$scope.buttons = [] //chả có gì cả, arduino gửi nhiêu thì nhận nhiêu!
-      	$scope.se =[2]
+      	$scope.se=[2]
 	////Khu 2 -- Cài đặt các sự kiện khi tương tác với người dùng
 	//các sự kiện ng-click, nhấn nút
 	$scope.updateSensor  = function() {
 		mySocket.emit("RAIN")
 	}
 	
-	$scope.upt5  = function(){
+	$scope.upt5  = function(se){
 		mySocket.emit("1")
 	}
-	$scope.upt51  = function(){
+	$scope.upt51  = function(se){
 		mySocket.emit("2")
 	}
-	$scope.upt6  = function(){
+	$scope.upt6  = function(se){
 		mySocket.emit("3")
 	}
-	$scope.upt61  = function(){
+	$scope.upt61  = function(se){
 		mySocket.emit("4")
 	}
-	$scope.upt7  = function(){
+	$scope.upt7  = function(se){
 		mySocket.emit("5")
 	}
-	$scope.upt71  = function(){
+	$scope.upt71  = function(se){
 		mySocket.emit("6")
 	}
-	$scope.upt8  = function(){
+	$scope.upt8  = function(se){
 		mySocket.emit("7")
 	}
-	$scope.upt81  = function(){
+	$scope.upt81  = function(se){
 		mySocket.emit("8")
 	}
 	
 	//Cách gửi tham số 1: dùng biến toàn cục! $scope.<tên biến> là biến toàn cục
-	$scope.changeLED = function() { 
-		 console.log("send LED ", $scope.leds_status)
-		
-		var json = {
-			"led": $scope.leds_status
-		}
-		mySocket.emit("LED", json)
-	}
 	
-	
-	////Khu 3 -- Nhận dữ liệu từ Arduno gửi lên (thông qua ESP8266 rồi socket server truyền tải!)
-	//các sự kiện từ Arduino gửi lên (thông qua esp8266, thông qua server)
-	mySocket.on('RAIN', function(json) {
-		$scope.CamBienMua = (json.digital == 1) ? "Không mưa" : "Có mưa rồi yeah ahihi"
-	})
-	
-	
-	
-	//// Khu 4 -- Những dòng code sẽ được thực thi khi kết nối với Arduino (thông qua socket server)
-	mySocket.on('connect', function() {
-		console.log("connected")
-		mySocket.emit("RAIN") //Cập nhập trạng thái mưa
-		
-		
-	})
 		
 });
